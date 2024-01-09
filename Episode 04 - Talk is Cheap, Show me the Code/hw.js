c6501ddb0fa2f0fca13fd467d35c4901103a90bc
mmj030703 <mayankjain7738@gmail.com>
2024-01-09 21:57:57 +0530

    Committed Episode 04

---
+++ Episode 04 - Talk is Cheap, Show me the Code/hw.js
@@ -0,0 +1,7 @@
+/*
+* HomeWork
+&   1) props
+&   2) Config Driven UI
+&   3) Optional Chaining (JS) Revise
+&   4) Key Prop in Component in JSX
+*/
---
+++ Episode 04 - Talk is Cheap, Show me the Code/notes.js
@@ -0,0 +1,23 @@
+/* 
+ ? Episode - 04 Talk is Cheap, Show me the Code
+ & In this Episode
+ &  - We learn how to Build any Frontend App.
+ &  - How to plan for building the Frontend App.
+ &  - We Created a Basic Food Ordering App (NamasteFood) UI Layout.
+ &  - How we can reuse Functional Components & how to do Component Composing.
+ &  - How to put dynamic data in Components.
+ &  - Importance of props in React, how to pass data to Component using props.
+ &  - How to do Object Destructuring, What is Optional Chaining in Objects.
+ &  - Learn about the Importance of Config(Configuration) Driven UI.
+ &  - Saw Swiggy API
+ &  - From the Swiggy API's Restaurant data generated our Restaurant Card. 
+ &  - Using map to render list of components. 
+ &  - Importance of key prop in Components.
+ */
+
+
+/*
+  & Notes:- 
+  & 
+  & Link: https://www.notion.so/e7f30291907f4db7a19629bea6fe1618?v=6d9f459c3db9468da3ccc81e5595f041
+*/
